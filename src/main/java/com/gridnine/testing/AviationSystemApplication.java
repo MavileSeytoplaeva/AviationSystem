9812359bc32566e6860a68d5f0ef64e6162d88d0
Мавиле Сейтоплаева <mavile.seitoplaeva@gmail.com>
2024-01-15 19:26:02 +0300

    init

---
+++ src/main/java/com/gridnine/testing/AviationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gridnine.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviationSystemApplication.class, args);
+	}
+
+}
